20156a0708490e5e77a551bb276314b3e61354e8
Arnob Talukder <arnob_talukder@hotmail.com>
2021-09-04 23:20:54 -0400

    init routs

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const PORT = process.env.PORT || 3333;
 
 const app = express();
 
+const sendEmailRouter = require("./routes/sendEmail");
+app.use("/send-email", sendEmailRouter);
+
+const bouncedEmailRouter = require("./routes/bouncedEmail");
+app.use("/bounced-email", bouncedEmailRouter);
+
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
